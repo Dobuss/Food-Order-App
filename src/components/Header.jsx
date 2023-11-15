55ac64a12cd9af45e12975be094418312d9644fb
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-15 16:12:16 +0200

    adding UI elements - buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import logoImg from '../assets/logo.jpg'
+import Button from './UI/Button'
 
 export default function Header(){
     return <header id="main-header">
@@ -7,7 +8,7 @@ export default function Header(){
             <h1>Food Order App</h1>
         </div>
         <nav>
-            <button>CART (0)</button>
+            <Button textOnly>Cart (0)</Button>
         </nav>
     </header>
 }
