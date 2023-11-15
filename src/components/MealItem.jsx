7039f5a353c0f5cd031c912adf64fae3395bce81
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-15 16:03:17 +0200

    added currency formatter util

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,13 +1,15 @@
+import { currencyFormatter } from "../util/formatting.js"
+
 export default function MealItem({meal}){
     return <li className="meal-item">
         <article>
             <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}/>
             <div>
             <h3>{meal.name}</h3>
-            <p className="meal-iitem-price">{meal.price}</p>
-            <p className="meal-iitem-description">{meal.description}</p>
+            <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
+            <p className="meal-item-description">{meal.description}</p>
             </div>
-            <p className="meal-iitem-actions">
+            <p className="meal-item-actions">
                 <button>Add to Cart</button>
             </p>
         </article>
