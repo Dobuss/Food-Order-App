80fe9b8ba7df349b6b842a9608ffa7c49385d4b7
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-15 17:11:35 +0200

    updating cart UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
+import { useContext } from 'react'
 import logoImg from '../assets/logo.jpg'
 import Button from './UI/Button'
+import CartContext from '../store/CartContext';
 
 export default function Header(){
+    const cartCtx = useContext(CartContext);
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity;
+    }, 0);
+
     return <header id="main-header">
         <div id="title">
             <img src={logoImg} alf="food"/>
             <h1>Yummy</h1>
         </div>
         <nav>
-            <Button textOnly>Cart (0)</Button>
+            <Button textOnly>Cart ({totalCartItems})</Button>
         </nav>
     </header>
 }
