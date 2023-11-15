ed8f8ede56ac467b6ecafcf90c2a097f501abd86
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-15 17:05:48 +0200

    adding cartContext and cart logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
     return <header id="main-header">
         <div id="title">
             <img src={logoImg} alf="food"/>
-            <h1>Food Order App</h1>
+            <h1>Yummy</h1>
         </div>
         <nav>
             <Button textOnly>Cart (0)</Button>
--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,18 +1,28 @@
-import { currencyFormatter } from "../util/formatting.js"
-import Button from "./UI/Button.jsx"
+import { useContext } from "react";
+import { currencyFormatter } from "../util/formatting.js";
+import Button from "./UI/Button.jsx";
+import CartContext from "../store/CartContext.jsx";
 
-export default function MealItem({meal}){
-    return <li className="meal-item">
-        <article>
-            <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}/>
-            <div>
-            <h3>{meal.name}</h3>
-            <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
-            <p className="meal-item-description">{meal.description}</p>
-            </div>
-            <p className="meal-item-actions">
-                <Button>Add to Cart</Button>
-            </p>
-        </article>
+export default function MealItem({ meal }) {
+  const cartCxt = useContext(CartContext);
+  function handleAddMealToCart() {
+    cartCxt.addItem(meal);
+  }
+  return (
+    <li className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+        <div>
+          <h3>{meal.name}</h3>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
+          <p className="meal-item-description">{meal.description}</p>
+        </div>
+        <p className="meal-item-actions">
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
+        </p>
+      </article>
     </li>
+  );
 }
